3b1c40235c75194613f8f0f31115032c813dbd99
Oliver Dahl <oliverpdahl@gmail.com>
2020-02-04 01:36:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -80,4 +80,14 @@ class Student
     end
   end
 
+  def self.first_X_students_in_grade_10
+    sql = <<-SQL 
+        SELECT * FROM students 
+        WHERE students.grade = 10
+        LIMIT ?
+    SQL 
+
+    DB[:conn]
+    
+
 end
